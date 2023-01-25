b64e3d4663f083722a3db401781a74ae4c61d8fd
Shawna C. Scott <shawnatyger@gmail.com>
2023-01-24 22:56:53 -0500

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :profiles
-  resources :campaigns
-  resources :events
-  resources :chapters
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
+  resources :campaigns
+  resources :chapters
   resources :characters
+  resources :events
+  resources :profiles
 
   # Defines the root path route ("/")
   root "profiles#show"
